dda521d98ea88a7280e538d95d9b8a2672fa2d48
alokskj <alokskj14@gmail.com>
2023-02-11 17:15:25 +0530

    version alpha 1.0

---
+++ src/date.js
@@ -0,0 +1,40 @@
+//jshint esversion:6
+
+exports.getDate = function() {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "short"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+};
+
+exports.getDay = function () {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+};
+
+exports.greet = ()=>{
+let timeNow = new Date().getHours();
+let greeting =
+  timeNow >= 5 && timeNow < 12
+    ? "Good Morning"
+    : timeNow >= 12 && timeNow < 18
+    ? "Good Afternoon"
+    : "Good evening";
+ return greeting
+
+}
+
